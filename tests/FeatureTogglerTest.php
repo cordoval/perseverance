5c582086a5611f0be95c6159b27b7fde029803f1
Luis Cordova <cordoval@gmail.com>
2014-04-18 00:17:57 -0500

    add some other dev files

---
+++ tests/FeatureTogglerTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Cordoval\Tests;
+
+class FeatureTogglerTest
+{
+    const OFF = false;
+    const ON = true;
+
+    protected $features;
+
+    public function __construct(array $features)
+    {
+        $this->features = $features;
+    }
+
+    public function isOff($feature)
+    {
+        return !$this->features[$feature];
+    }
+
+    public function isOn($feature)
+    {
+        return $this->features[$feature];
+    }
+
+    public function turnOn($feature)
+    {
+        $this->features[$feature] = self::ON;
+    }
+
+    public function turnOff($feature)
+    {
+        $this->features[$feature] = self::OFF;
+    }
+}
